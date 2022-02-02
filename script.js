0d7f9ca22ed0e0041ec4fa7b3a0e8b27eb42dcbf
=Eric Niemeyer <=niemeyer.eric@gmail.com>
2022-02-02 11:46:48 -0500

    Separate Likes but Not Perfect

--- script.js
+++ script.js
@@ -21,3 +21,21 @@ function mouseoverf(element) {
 function mouseoutf(element) {
     element.style.backgroundColor = "grey";
 }
+
+var users = [
+    { name: "Neil M", likes: 9 },
+    { name: "Nichole K", likes: 12 },
+    { name: "Jim R", likes: 9 },
+];
+var likePage1 = document.querySelector(".likeCount1");
+var likePage2 = document.querySelector(".likeCount2");
+var likePage3 = document.querySelector(".likeCount3");
+function likeButtonFunction(userId) {
+    // alert("NEW NEW NEW")
+
+    users[userId].likes++;
+    
+    likePage1.innerHTML = users[0].likes;
+    likePage2.innerHTML = users[1].likes;
+    likePage3.innerHTML = users[2].likes;
+}
